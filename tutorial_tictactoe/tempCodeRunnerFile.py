7112ae56fcd200911a32be45001467de2823909e
blockhead4 <blockhead4@naver.com>
2019-07-01 19:14:13 +0900

    add

--- tutorial_tictactoe/tempCodeRunnerFile.py
+++ tutorial_tictactoe/tempCodeRunnerFile.py
@@ -1,2 +1,7 @@
-]) == len(check) and check[0] != 0:
-    print("Winner!")
+ame_size = 3
+game = [[0 for row in range(game_size)] for col in range(game_size)]
+row = input("row = ")
+col = input("col = ")
+player = next(itertools.cycle([1, 2]))
+ppp = game_map(game, row, col, player)
+play = False
--- tutorial_tictactoe/tutorial_practice.py
+++ tutorial_tictactoe/tutorial_practice.py
@@ -1,16 +1,19 @@
 import itertools 
-
-game = [[2, 1, 1],
-        [1, 1, 0],
-        [1, 0, 2]]
+from colorama import Fore, Back, Style, init
+init()
 
 def win(current_game):
 
+    def all_same(list):
+        if list.count(list[0]) == len(list) and list[0] != 0:
+            return True
+        else:
+            return False
+    
     # horizontal    
-    for i, row in enumerate(game):
-        print(i, row)
-        if row.count(row[0]) == len(row) and row[0] != 0:
-            print('player ?? is the winner horizontally!! (-)') 
+    for row in game:
+        if all_same(row)
+            print(f"player {row[0]} is the winner horizontally!! (-)") 
             return True
 
     # vertical
@@ -18,32 +21,86 @@ def win(current_game):
         check = []
         for row in game:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != 0:
-            print('player ?? is the winner vertically!! (-)') 
+        if all_same(check)
+            print(f'player {check[0]} is the winner vertically!! (-)') 
             return True
 
     # diagonal
     diags = []
     for row, col in enumerate(reversed(range(len(game)))):
         diags.append(game[row][col])    
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
-        print('player ?? is the winner diagnolly!! (/)')
+    if all_same(diags)
+        print(f'player {diags[0]} is the winner diagnolly!! (/)')
         return True 
 
     diags = []
     for row, col in enumerate(range(len(game))):
         diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
-        print('player ?? is the winner diagnolly!! (\\)')
+    if all_same(diags)
+        print(f'player {diags[0]} is the winner diagnolly!! (\\)')
         return True
 
     return False
 
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    
+    try: 
+        if game_map[row][column] != 0:
+            print("This position is already occupado!, Choose another!")
+            return game_map, False
 
-def game_board(plyaer=0, row=0, column=0, just_display=False):
+        print("   "+ "  ".join([str(i) for i in range(len(game))]))
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            colored_row = ""
+            for item in row:
+                if item == 0:
+                    colored_row += "   "
+                elif item == 1:
+                    colored_row += Fore.GREEN + " O " + Style.RESET_ALL
+                elif item == 2:
+                    colored_row += Fore.RED + " X " + Style.RESET_ALL
+            print(count, colored_row)
+        
+    except IndexError as e:
+        print("Error : Make sure you input row/column as range of game?", e)
+        return game_map, False
 
-    print("   "+ "  ".join([str(i) for i in range(len(game))]))
-    for i, row in enumerate(game):
-        print(i, row)
+    except Exception as e:
+        print("Something went very wrong!!!", e)
+        return game_map, False
 
-game_board()
+    return game_map, True
+    
+play = True
+players = [1, 2]
+while play:
+    game_size = int(input("what size game of tic-tac-toe do you want to play? "))
+    game = [[0 for row in range(game_size)] for col in range(game_size)]
+    game_won = False
+    game, _ = game_board(game, just_display=True)
+    player_choice = itertools.cycle(players)
+
+    while not game_won:
+        current_player = next(player_choice)
+        print(f"Curruent player : {current_player}")
+        played = False   # 이 부분이 왜 필요한 것인지???
+
+        while not played:
+            row_choice = int(input("what row do you want to play? "))
+            column_choice = int(input("what column do you want to play? "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
+
+        if win(game):
+            game_won = True
+            play_again = input("The game is over, would you like to play again? (y/n) ")
+            if play_again.lower() == "y":
+                print("Restarting tic-tac-toe!")
+            elif play_again.lower() == "n":
+                print("Byeeeeeeeeee~!")
+                play = False
+            else:
+                print("Not a valid answer... , so c u l8ter aligator!")
+                play = False
+
